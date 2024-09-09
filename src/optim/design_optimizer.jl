f20a7bd1155a233779f436f9aa784cf56b50451c
Ben Fuller <ben.fuller@conexusinc.com>
2024-09-09 17:06:31 -0600

    added better initializer using dirichlet; moved some code to optimizer where it belongs

--- src/optim/design_optimizer.jl
+++ src/optim/design_optimizer.jl
@@ -3,6 +3,58 @@ module DesignOptimizer
 using SpecialFunctions
 using LinearAlgebra
 using ThreadsX
+using LinearAlgebra
+using Optim
+using ForwardDiff
+using Optimization, OptimizationNLopt
+
+function create_univariate_objective(X, row, d, obj_crit)
+    og_row = copy(X[row, :])
+    function univariate_obj(x)
+        X[row, :] .= og_row .+ x * d
+        score = obj_crit(X)
+        X[row, :] .= og_row
+        return score
+    end
+    return univariate_obj
+end
+
+function bounded_univariate_optimizer(X, row, d, obj_crit)
+    univariate_obj = create_univariate_objective(X, row, d, obj_crit)
+    result = Optim.optimize(univariate_obj, 0, 1.0)
+    optim_point = X[row, :] .+ Optim.minimizer(result) * d
+    return optim_point, Optim.minimum(result)
+end
+
+function bounded_univariate_gradient_optimizer(X, row, d, obj_crit; optimizer=NLopt.LD_BFGS)
+    # Make univariate optimizer
+    univariate_obj = create_univariate_objective(X, row, d, obj_crit)
+    univariate_obj_vec = (x, _) -> univariate_obj(x[1])
+
+    # Initial values and bounds
+    x0 = [0]
+    p=[0]
+    lb = [0]
+    ub = [1.0]
+
+    # Define optimization problem
+    f = OptimizationFunction(univariate_obj_vec, Optimization.AutoForwardDiff())
+    prob = OptimizationProblem(f, x0, p, ub=ub, lb=lb)
+    solution = solve(prob, optimizer())
+    optim_point = X[row, :] .+ solution.u[1] * d
+
+    # Evaluate
+    tmp = copy(X[row, :])
+    X[row, :] .= optim_point
+    score = obj_crit(X)
+    X[row, :] .= tmp
+
+    return optim_point, score
+end
+
+function bounded_univariate_gradient_optimizer(optimizer=NLopt.LD_BFGS)
+    return (X, row, d, obj_crit) -> bounded_univariate_gradient_optimizer(X, row, d, obj_crit; optimizer=optimizer)
+end
 
 function obj_crit_line(t, X, row, d, obj_crit)
     # Compute the candidate design point
